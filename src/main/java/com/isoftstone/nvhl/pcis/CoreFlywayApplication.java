8d4fcee3d20aa553c173e27192080bb2fa1780ea
777yL <pqrenc@isoftstone.com>
2020-10-29 16:32:42 +0800

    core-flyway

---
+++ src/main/java/com/isoftstone/nvhl/pcis/CoreFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.isoftstone.nvhl.pcis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreFlywayApplication.class, args);
+    }
+
+}
